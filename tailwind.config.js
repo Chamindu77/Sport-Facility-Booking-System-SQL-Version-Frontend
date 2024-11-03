81be3420c80e538af2379f8bbf630d60aaf46ffc
chamindu77 <chamindunipun99@gmail.com>
2024-11-04 05:11:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      colors: {
+        'custom-gray': '#D8D8D8',
+        'custom-hover': '#1f2937',
+        'custom-blue': '#555A5D',
+    },
+    },
+  },
+  plugins: [],
+}
+
